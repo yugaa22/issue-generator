a20148a28edfa17c2404bcee462f9f44e12a45bf
Sagayaraj <sagayaraj.d@opsmx.io>
2024-03-13 15:13:04 +0530

    Pre and Post install script

--- build-deb/issue-gen/opt/apps/issue-gen/postinst.sh
+++ build-deb/issue-gen/opt/apps/issue-gen/postinst.sh
@@ -1,5 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash
+#Post-install script
 
-# Post-installation script
+#/opt/apps/issue-gen/issuegen-start.sh
 
-echo "Post-install : Nothing configured"
+sleep 30s
+echo "Starting application ..."
+cd /opt/apps/issue-gen
+LOG_FILE="issuegen.log"
+nohup java -jar issuegen-0.1.jar </dev/null &> $LOG_FILE &
+sleep 1m
+
+# Check if the log file contains text X, Y, or Z
+if grep -qE 'BindException|ERROR Failed to start' "$LOG_FILE"; then
+    echo "Application failed to start. Found error text in the App log file."
+    exit 1  # Exit with error code 1
+else
+    echo "Application started successfully."
+    exit 0  # Exit peacefully with code 0
+fi
--- build-deb/issue-gen/opt/apps/issue-gen/preinst.sh
+++ build-deb/issue-gen/opt/apps/issue-gen/preinst.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -x
 
 # Pre-installation script
+# Used for manual run. It cannot be called automatically by .deb install as the scriptfile will not be available prior to installation
 
-echo "Pre-install : Nothing configured"
+sleep 180s
+#cd /opt/apps/issue-gen
+proc=`ps -ef|grep issuegen |grep -v grep |awk '{ print $2}'`
+if [ -z "$proc" ]; then
+   echo "App is not running. Proceeding with installation ..."
+else
+   echo "App is running. Stopping it before installation ..."
+   kill -9 $proc
+   DTSTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
+   LOG_FILE="issuegen.log"
+   cd /opt/apps/issue-gen
+   cp -v $LOG_FILE $LOG_FILE_$DTSTAMP.log
+fi
+
+#echo "Pre-install : Nothing configured"
+echo "Pre-install script : done"
